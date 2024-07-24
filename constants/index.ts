53ddf3b8d2f7f5d93a4bd3aeaa3d175b17331d27
Jezebel1990 <jezebelguedes@gmail.com>
2024-07-24 01:33:37 -0300

    add explore section

--- constants/index.ts
+++ constants/index.ts
@@ -3,7 +3,47 @@
 export const exploreWorlds = [
     {
         id: 'world-1',
-        imgUrl: '/public/planet-01.png',
+        imgUrl: '/planet-01.png',
         title: 'Realidade Virtual',
-    }
+    }, 
+    {
+        id: 'world-2',
+        imgUrl: '/planet-02.png',
+        title: 'Satoru Gojo',
+    }, 
+    // {
+    //     id: 'world-3',
+    //     imgUrl: '/planet-03.png',
+    //     title: 'Autônomo ',
+    // }, 
+    {
+        id: 'world-4',
+        imgUrl: '/planet-04.png',
+        title: 'Digital ',
+    }, 
+    // {
+    //     id: 'world-5',
+    //     imgUrl: '/planet-05.png',
+    //     title: 'Cidade Virtual ',
+    // }, 
+    {
+        id: 'world-6',
+        imgUrl: '/planet-06.png',
+        title: 'Avatar',
+    }, 
+    // {
+    //     id: 'world-7',
+    //     imgUrl: '/planet-07.png',
+    //     title: 'Disney',
+    // }, 
+    // {
+    //     id: 'world-8',
+    //     imgUrl: '/planet-08.png',
+    //     title: 'NFTs',
+    // }, 
+    {
+        id: 'world-9',
+        imgUrl: '/planet-09.png',
+        title: 'Holográfico',
+    }, 
 ]
