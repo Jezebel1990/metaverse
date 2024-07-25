0dcfea5011ef31058ace2085678d6422d5c107f1
Jezebel1990 <jezebelguedes@gmail.com>
2024-07-24 22:23:25 -0300

    add section get started

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import  Navbar  from '../components/Navbar';
 import Explore  from '../sections/Explore';
 import Hero from '../sections/Hero';
 import About from '../sections/About';
+import GetStarted from '../sections/GetStarted';
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
@@ -11,6 +12,9 @@ const Page = () => (
  <About />
  <div className="gradient-03 z-0" />
  <Explore />
+ </div>
+ <div className="relative">
+<GetStarted />
  </div>
   </div>
 );
--- constants/index.ts
+++ constants/index.ts
@@ -11,31 +11,16 @@ export const exploreWorlds = [
         imgUrl: '/planet-02.png',
         title: 'Satoru Gojo',
     }, 
-    // {
-    //     id: 'world-3',
-    //     imgUrl: '/planet-03.png',
-    //     title: 'Autônomo ',
-    // }, 
     {
         id: 'world-4',
         imgUrl: '/planet-04.png',
         title: 'Digital ',
     }, 
-    // {
-    //     id: 'world-5',
-    //     imgUrl: '/planet-05.png',
-    //     title: 'Cidade Virtual ',
-    // }, 
     {
         id: 'world-6',
         imgUrl: '/planet-06.png',
         title: 'Avatar',
     }, 
-    // {
-    //     id: 'world-7',
-    //     imgUrl: '/planet-07.png',
-    //     title: 'Disney',
-    // }, 
     // {
     //     id: 'world-8',
     //     imgUrl: '/planet-08.png',
@@ -46,4 +31,71 @@ export const exploreWorlds = [
         imgUrl: '/planet-09.png',
         title: 'Holográfico',
     }, 
-]
+];
+
+export const startingFeatures = [
+ 'Encontre um mundo que combina com você e no qual deseja entrar',
+  'Entre no mundo lendo para estar seguro',
+  'Não há necessidade de rodeios, apenas pise no acelerador e divirta-se',
+];
+
+
+export const newFeatures = [
+    {
+      imgUrl: '/vrpano.svg',
+      title: 'Um novo mundo',
+      subtitle:
+          'Temos a atualização mais recente com o novo mundo para você experimentar',
+    },
+    {
+      imgUrl: '/headset.svg',
+      title: 'Mais realista',
+      subtitle:
+          'Na última atualização, seus olhos ficarão estreitos, tornando o mundo mais real do que nunca',
+    },
+  ];
+
+  export const insights = [
+    {
+      imgUrl: '/planet-03.png',
+      title: 'O lançamento do Metaverso automotivo',
+      subtitle:
+          'O objetivo deste carro é que as pessoas a bordo possam viajar não apenas no mundo físico mas também nos virtuais.',
+    },
+    {
+      imgUrl: '/planet-05.png',
+      title: 'As cidades futuristas no metaverso',
+      subtitle:
+          'Mais que um espaço digital, o metaverso é um ambiente virtual onde as pessoas podem interagir e fazer atividades típicas do mundo físico.',
+    },
+    {
+      imgUrl: '/planet-07.png',
+      title: 'Uma aventura marítima: Disney Uncharted Adventure',
+      subtitle:
+          'Esta é, uma jornada multidimensional com os personagens mais icônicos da Disney e da Pixar, como Moana, Tiana, Peter Pan, Nemo, entre outros.',
+    },
+  ];
+  
+  
+
+
+
+
+  export const socials = [
+    {
+      name: 'twitter',
+      url: '/twitter.svg',
+    },
+    {
+      name: 'linkedin',
+      url: '/linkedin.svg',
+    },
+    {
+      name: 'instagram',
+      url: '/instagram.svg',
+    },
+    {
+      name: 'facebook',
+      url: '/facebook.svg',
+    },
+  ];
